68abd13183529a70945c742b489b6b3fb6fde758
kl mithunvel <147433270+KL-Mithunvel@users.noreply.github.com>
2025-03-04 06:50:06 +0530

    Update rproj.r

--- lab/rproj.r
+++ lab/rproj.r
@@ -1,1 +1,5 @@
-
+data=weather_2025_02_18
+summary(data)
+plot(data$temp,type="l",main="Ambient temperature",xlab="sno",ylab="temp",col="blue")
+plot(data$humidity,type="l",main="Ambient Humidity",xlab="sno",ylab="humidity",col="red")
+plot(data$'cpu temp',type="l",main="CPU temperature",xlab="sno",ylab="cpu temp",col="yellow")
