e31e24a83839cf070b3a94fe2536953054bb67c3
kl mithunvel <147433270+KL-Mithunvel@users.noreply.github.com>
2025-04-04 08:37:51 +0530

    Update 2qn.r

--- lab/2qn.r
+++ lab/2qn.r
@@ -1,15 +1,10 @@
-
 data= weather_2025_02_18
-data
 #regression b/w temp, humidity and cpu temp
 x=data$temp
-x
+
 y= data$humidity
-y
 z= data$`cpu temp`
-z
 r=lm(z~x+y)
-r
 summary(r)
 
 library(scatterplot3d)
